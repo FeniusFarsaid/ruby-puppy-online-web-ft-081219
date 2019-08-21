565a32738a7da08e9603f74fe0fac6ef8ce55d33
David Ingram <david@ingramaviation.com>
2019-08-21 17:23:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,31 @@
 # Add your code here
+class Dog 
+  
+  attr_accessor :name 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self
+  end 
+  
+  def self.all
+		@@all.each do |dog|
+			puts dog.name
+		end
+	end
+	
+	def print_all
+    print @@all
+ 	end 
+	 
+# 	def save(dog)
+# 	  @@all << self.dog
+# 	 end 
+  
+  def self.clear_all
+		@@all.clear
+	end
+end 
+
